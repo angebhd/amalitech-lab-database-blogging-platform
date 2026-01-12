86ee2c6e54b83768c261b42e91127aa006205f38
Ange A. Buhendwa <mickaelbhd@gmail.com>
2026-01-12 15:28:43 +0200

    chore: add env example

---
+++ src/main/java/amalitech/blog/model/BaseEntity.java
@@ -0,0 +1,15 @@
+package amalitech.blog.model;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class BaseEntity {
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+  private LocalDateTime deletedAt;
+  private boolean isDeleted;
+
+}
